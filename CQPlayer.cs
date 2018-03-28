8c8e52a62eaa4cc9c7de8ae4e77e1594378d9190
lbryan212 <lbryan212@gmail.com>
2018-03-28 18:47:17 -0400

    Created CQPlayer.cs
    Creating custom controller based off ported code.

---
+++ CQPlayer.cs
@@ -0,0 +1,262 @@
+* Just some side notes here.
+ *
+ * - Should keep in mind that idTech's cartisian plane is different to Unity's:
+ *    Z axis in idTech is "up/down" but in Unity Z is the local equivalent to
+ *    "forward/backward" and Y in Unity is considered "up/down".
+ *
+ * - Code's mostly ported on a 1 to 1 basis, so some naming convensions are a
+ *   bit fucked up right now.
+ *
+ * - UPS is measured in Unity units, the idTech units DO NOT scale right now.
+ *
+ * - Default values are accurate and emulates Quake 3's feel with CPM(A) physics.
+ *
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Contains the command the user wishes upon the character
+struct Cmd
+{
+    public float forwardMove;
+    public float rightMove;
+    public float upMove;
+}
+
+public class CQPlayer : MonoBehaviour
+{
+    public Transform playerView;     // Camera
+    public float playerViewYOffset = 0.6f; // The height at which the camera is bound to
+    public float xMouseSensitivity = 30.0f;
+    public float yMouseSensitivity = 30.0f;
+//
+    /*Frame occuring factors*/
+    public float gravity = 20.0f;
+
+    public float friction = 6; //Ground friction
+
+    /* Movement stuff */
+    public float moveSpeed = 7.0f;                // Ground move speed
+    public float runAcceleration = 14.0f;         // Ground accel
+    public float runDeacceleration = 10.0f;       // Deacceleration that occurs when running on the ground
+    public float airAcceleration = 2.0f;          // Air accel
+    public float airDecceleration = 2.0f;         // Deacceleration experienced when ooposite strafing
+    public float airControl = 0.3f;               // How precise air control is
+    public float sideStrafeAcceleration = 50.0f;  // How fast acceleration occurs to get up to sideStrafeSpeed when
+    public float sideStrafeSpeed = 1.0f;          // What the max speed to generate when side strafing
+    public float jumpSpeed = 8.0f;                // The speed at which the character's up axis gains when hitting jump
+    public float moveScale = 1.0f;
+
+    /*print() style */
+    public GUIStyle style;
+
+    /*FPS Stuff */
+    public float fpsDisplayRate = 4.0f; // 4 updates per sec
+
+    private int frameCount = 0;
+    private float dt = 0.0f;
+    private float fps = 0.0f;
+
+    private CharacterController _controller;
+
+    // Camera rotations
+    private float rotX = 0.0f;
+    private float rotY = 0.0f;
+
+    private Vector3 moveDirectionNorm = Vector3.zero;
+    private Vector3 playerVelocity = Vector3.zero;
+    private float playerTopVelocity = 0.0f;
+
+    // Q3: players can queue the next jump just before he hits the ground
+    private bool wishJump = false;
+
+    // Used to display real time fricton values
+    private float playerFriction = 0.0f;
+
+    // Player commands, stores wish commands that the player asks for (Forward, back, jump, etc)
+    private Cmd _cmd;
+
+    private void Start()
+    {
+        
+        // Get animator component
+        animator = GetComponent<Animator>();
+        
+        // Hide the cursor
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+        // Put the camera inside the capsule collider
+        playerView.position = new Vector3(
+            transform.position.x,
+            transform.position.y + playerViewYOffset,
+            transform.position.z);
+
+        _controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        // Do FPS calculation
+        frameCount++;
+        dt += Time.deltaTime;
+        if (dt > 1.0 / fpsDisplayRate)
+        {
+            fps = Mathf.Round(frameCount / dt);
+            frameCount = 0;
+            dt -= 1.0f / fpsDisplayRate;
+                 }
+        /* Ensure that the cursor is locked into the screen */
+        if (Cursor.lockState != CursorLockMode.Locked) {
+            if (Input.GetButtonDown("Fire1"))
+                Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        /* Camera rotation stuff, mouse controls this shit */
+        rotX -= Input.GetAxisRaw("Mouse Y") * xMouseSensitivity * 0.02f;
+        rotY += Input.GetAxisRaw("Mouse X") * yMouseSensitivity * 0.02f;
+
+        // Clamp the X rotation
+        if(rotX < -90)
+            rotX = -90;
+        else if(rotX > 90)
+            rotX = 90;
+
+        this.transform.rotation = Quaternion.Euler(0, rotY, 0); // Rotates the collider
+        playerView.rotation     = Quaternion.Euler(rotX, rotY, 0); // Rotates the camera
+
+        
+
+        /* Movement, here's the important part */
+        QueueJump();
+        if(_controller.isGrounded)
+            GroundMove();
+        else if(!_controller.isGrounded)
+            AirMove();
+
+        // Move the controller
+        _controller.Move(playerVelocity * Time.deltaTime);
+
+        /* Calculate top velocity */
+        Vector3 udp = playerVelocity;
+        udp.y = 0.0f;
+        if(playerVelocity.magnitude > playerTopVelocity)
+            playerTopVelocity = playerVelocity.magnitude;
+
+        //Need to move the camera after the player has been moved because otherwise the camera will clip the player if going fast enough and will always be 1 frame behind.
+        // Set the camera's position to the transform
+        playerView.position = new Vector3(
+            transform.position.x,
+            transform.position.y + playerViewYOffset,
+            transform.position.z);
+    }
+
+     /*******************************************************************************************************\
+    |* MOVEMENT
+    \*******************************************************************************************************/
+
+    /**
+     * Sets the movement direction based on player input
+     */
+    private void SetMovementDir()
+    {
+        _cmd.forwardMove = Input.GetAxisRaw("Vertical");
+        _cmd.rightMove = Input.GetAxisRaw("Horizontal");
+        // Create movement parameters in animator
+        animator.SetFloat("Forward",_cmd.forwardMove);
+        animator.SetFloat("Strafe",_cmd.rightMove);
+    }
+    
+    
+   private void WeaponAnims()
+    {
+    // Weapon anims
+        bool mouse1 = Input.GetButtonDown("Fire1");
+        bool mouse2 = Input.GetButtonDown("Fire2");
+        // Create movement parameters in animator
+        animator.SetBool("Shoot",mouse1);
+        animator.SetBool("Scope",mouse2);
+       
+       
+       
+       
+       
+       
+       
+    }
+
+
+
+
+
+
+
+    /**
+     * Queues the next jump just like in Q3
+     */
+    private void QueueJump()
+    {
+        if(Input.GetButtonDown("Jump") && !wishJump)
+            wishJump = true;
+        if(Input.GetButtonUp("Jump"))
+            wishJump = false;
+    }
+
+    /**
+     * Execs when the player is in the air
+    */
+    private void AirMove()
+    {
+        Vector3 wishdir;
+        float wishvel = airAcceleration;
+        float accel;
+
+        float scale = CmdScale();
+
+        SetMovementDir();
+
+        wishdir =  new Vector3(_cmd.rightMove, 0, _cmd.forwardMove);
+        wishdir = transform.TransformDirection(wishdir);
+
+        float wishspeed = wishdir.magnitude;
+        wishspeed *= moveSpeed;
+
+        wishdir.Normalize();
+        moveDirectionNorm = wishdir;
+        wishspeed *= scale;
+
+        // CPM: Aircontrol
+        float wishspeed2 = wishspeed;
+        if (Vector3.Dot(playerVelocity, wishdir) < 0)
+            accel = airDecceleration;
+        else
+            accel = airAcceleration;
+        // If the player is ONLY strafing left or right
+        if(_cmd.forwardMove == 0 && _cmd.rightMove != 0)
+        {
+            if(wishspeed > sideStrafeSpeed)
+                wishspeed = sideStrafeSpeed;
+            accel = sideStrafeAcceleration;
+        }
+
+        Accelerate(wishdir, wishspeed, accel);
+        if(airControl > 0)
+            AirControl(wishdir, wishspeed2);
+        // !CPM: Aircontrol
+
+        // Apply gravity
+        playerVelocity.y -= gravity * Time.deltaTime;
+    }
+
+    /**
+     * Air control occurs when the player is in the air, it allows
+     * players to move side to side much faster rather than being
+     * 'sluggish' when it comes to cornering.
+     */
+    private void AirControl(Vector3 wishdir, float wishspeed)
+    {
+        float zspeed;
+        float speed;
+        float dot;
